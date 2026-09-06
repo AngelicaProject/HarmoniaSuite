f50821df59b2f0578559807f75e7bc15ac307804
pokeda <87545780+xchgeax@users.noreply.github.com>
2026-09-07 02:51:19 +0300

    fix(tray): use native AWT popup menu instead of styled Swing

--- src/main/java/com/harmoniasuite/service/TrayService.java
+++ src/main/java/com/harmoniasuite/service/TrayService.java
@@ -3,17 +3,13 @@ package com.harmoniasuite.service;
 import com.harmoniasuite.config.StartupLink;
 import jakarta.annotation.PreDestroy;
 import java.awt.GraphicsEnvironment;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.imageio.ImageIO;
-import javax.swing.JDialog;
-import javax.swing.JMenuItem;
-import javax.swing.JPopupMenu;
-import javax.swing.UIManager;
-import javax.swing.event.PopupMenuEvent;
-import javax.swing.event.PopupMenuListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -46,64 +42,27 @@ public class TrayService implements ApplicationListener<ApplicationReadyEvent> {
             return;
         }
         try {
-            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
             var image = ImageIO.read(getClass().getResourceAsStream("/static/img/yuki-icon.png"));
             if (image == null) {
                 return;
             }
-            JDialog owner = new JDialog();
-            owner.setUndecorated(true);
-            owner.setAlwaysOnTop(true);
-            JPopupMenu menu = new JPopupMenu();
-            JMenuItem open = new JMenuItem("Открыть в браузере");
+            PopupMenu menu = new PopupMenu();
+            MenuItem open = new MenuItem("Открыть в браузере");
             open.addActionListener(e -> StartupLink.openBrowser(link()));
-            JMenuItem exit = new JMenuItem("Выйти");
+            MenuItem exit = new MenuItem("Выйти");
             exit.addActionListener(e -> close());
             menu.add(open);
+            menu.addSeparator();
             menu.add(exit);
-            menu.addPopupMenuListener(new PopupMenuListener() {
-                @Override
-                public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
-                    owner.setVisible(false);
-                }
-
-                @Override
-                public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
-                }
-
-                @Override
-                public void popupMenuCanceled(PopupMenuEvent e) {
-                    owner.setVisible(false);
-                }
-            });
             icon = new TrayIcon(image, "Harmonia Suite");
             icon.setImageAutoSize(true);
+            icon.setPopupMenu(menu);
             icon.addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
                     if (e.getButton() == MouseEvent.BUTTON1) {
                         StartupLink.openBrowser(link());
                     }
-                }
-
-                @Override
-                public void mousePressed(MouseEvent e) {
-                    if (e.isPopupTrigger()) {
-                        showMenu(e);
-                    }
-                }
-
-                @Override
-                public void mouseReleased(MouseEvent e) {
-                    if (e.isPopupTrigger()) {
-                        showMenu(e);
-                    }
-                }
-
-                private void showMenu(MouseEvent e) {
-                    owner.setLocation(e.getXOnScreen(), e.getYOnScreen() - menu.getPreferredSize().height);
-                    owner.setVisible(true);
-                    menu.show(owner, 0, 0);
                 }
             });
             SystemTray.getSystemTray().add(icon);
