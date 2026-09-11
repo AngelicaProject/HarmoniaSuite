b3bc3fab0acf5ab3a321855a409fd4450cacd033
pokeda <87545780+xchgeax@users.noreply.github.com>
2026-09-11 21:58:41 +0300

    fix(installer): use job object API module

--- apps/installer/src/process.rs
+++ apps/installer/src/process.rs
@@ -325,11 +325,10 @@ mod windows_job {
 
     use windows_sys::Win32::Foundation::{CloseHandle, HANDLE};
     use windows_sys::Win32::System::JobObjects::{
-        AssignProcessToJobObject, JobObjectExtendedLimitInformation, SetInformationJobObject,
-        TerminateJobObject, JOBOBJECT_EXTENDED_LIMIT_INFORMATION,
+        AssignProcessToJobObject, CreateJobObjectW, JobObjectExtendedLimitInformation,
+        SetInformationJobObject, TerminateJobObject, JOBOBJECT_EXTENDED_LIMIT_INFORMATION,
         JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE,
     };
-    use windows_sys::Win32::System::Threading::CreateJobObjectW;
 
     pub struct Job {
         handle: HANDLE,
